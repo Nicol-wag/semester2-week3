b557bb352e2e773b2beea61f6cad306b785d2923
Nicol-wag <nhbd0465@leeds.ac.uk>
2026-02-21 01:09:21 +0000

    work completed

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,30 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name:Duoyi Wang
+ * ID:201930782
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+   double salary = 36250.0;
+   double ni_rate = 0.08;
+   double tax_rate = 0.15;
 
     // calculate the deductions and final take-home salary
-
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+   double ni_cont = salary * ni_rate;
+   double salary_after_ni = salary - ni_cont;
+   double taxable_amt =  salary_after_ni - 12500;
+   double tax_cont = taxable_amt * tax_rate;
+   double take_home_salary = salary_after_ni - tax_cont;
+
+   
+   printf("Salary £%.2f\n", salary);
+   printf("NI contribution £%.2f\n", ni_cont);
+   printf("Tax contribution £%.2f\n", tax_cont);
+   printf("Take home salary £%.2f\n", take_home_salary);
 
     return 0;
  }
